c53c6e10d0a2226b1c0de6a44976ff417a0aa12a
Anwar <anmdov@gmail.com>
2020-12-31 02:21:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//1--
 // You are asked to square every digit of a number and concatenate them.
 // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 // Note: The function accepts an integer and returns an integer
@@ -8,3 +9,47 @@ function squareDigits(num) {
   let parsed = square.join("") ;
   return parseInt(parsed, 0);
 }
+
+
+//2---
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+function isIsogram(str) {
+  let sbStr = str.toLowerCase().split("");
+  let strEv = sbStr.every((c, i) => sbStr.indexOf(c) === i);
+   if ( strEv === true || str === "") {
+     return true;
+   }
+   return false;
+}
+
+isIsogram("Dermatoglyphics");
+
+
+// const uniqueArr = [];
+// dataArr.forEach((item)=>{
+//     //pushes only unique element
+//       if(!uniqueArr.includes(item)){
+//         uniqueArr.push(item);
+//       }
+   
+// })
+
+
+
+
+// function isIsogram(str){
+//   let sbStr = str.split(''); 
+//   // let indStr = sbStr.map((data, index) => {
+//   //   return index; })
+//   // console.log(indStr);
+//   //splitting strings into array so we can iterate
+//   let removed = sbStr.filter((data, index) => {
+//     return sbStr.indexOf(data) === index;
+//   })
+//   console.log(sbStr === removed)
+//   // if (indStr === removed || str === "") {
+//   //   return true; } 
+//   //   else {
+//   //     return false; }
+// }
